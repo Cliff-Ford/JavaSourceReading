774c55f190cb48e0acb177a44fd9ca46963c9c54
Cliff-Ford <473375811@qq.com>
2019-10-20 00:57:22 +0800

    调整项目结构

--- src/test/java/use/java/lang/UseString.java
+++ src/test/java/use/java/lang/UseString.java
@@ -1,32 +1,32 @@
 package use.java.lang;
 
-import org.junit.Assert;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
-
+@Slf4j
 public class UseString {
 
     @Test
     public void testConstructor() throws UnsupportedEncodingException {
-        System.out.println("开始测试String的构造方法");
-        System.out.println(new String());
-        System.out.println(new String("original"));
-        System.out.println(new String(new char[]{'a','b','c'}));
-        System.out.println(new String(new char[]{'a','b','c'},1,2));
-        System.out.println(new String(new int[]{324324,43,243,987654},1,3));
-        System.out.println(new String(new byte[]{'a','b','c'},0,0,3));
-        System.out.println(new String(new byte[]{'a','b','c'},0));
-        System.out.println(new String(new byte[]{'a','b','c'},0,3,"UTF8"));
-        System.out.println(new String(new byte[]{'a','b','c'},0,3, StandardCharsets.ISO_8859_1));
-        System.out.println(new String(new byte[]{'a','b','c'},"utf8"));
-        System.out.println(new String(new byte[]{'a','b','c'}, StandardCharsets.UTF_8));
-        System.out.println(new String(new byte[]{'a','b','c'}, 0,3));
-        System.out.println(new String(new byte[]{'a','b','c'}));
-        System.out.println(new String(new StringBuffer("abc")));
-        System.out.println(new String(new StringBuilder("abc")));
-        System.out.println("测试String的构造方法结束");
+        log.info("测试String的构造方法开始");
+        log.info(new String());
+        log.info(new String("original"));
+        log.info(new String(new char[]{'a','b','c'}));
+        log.info(new String(new char[]{'a','b','c'},1,2));
+        log.info(new String(new int[]{324324,43,243,987654},1,3));
+        log.info(new String(new byte[]{'a','b','c'},0,0,3));
+        log.info(new String(new byte[]{'a','b','c'},0));
+        log.info(new String(new byte[]{'d','d','d'},0,3,"UTF8"));
+        log.info(new String(new byte[]{'a','b','c'},0,3, StandardCharsets.ISO_8859_1));
+        log.info(new String(new byte[]{'f','f','c'},"utf8"));
+        log.info(new String(new byte[]{'a','b','c'}, StandardCharsets.UTF_8));
+        log.info(new String(new byte[]{'a','b','c'}, 0,3));
+        log.info(new String(new byte[]{'a','b','c'}));
+        log.info(new String(new StringBuffer("abcBuffer")));
+        log.info(new String(new StringBuilder("abcBuilder")));
+        log.info("测试String的构造方法结束");
     }
 }
