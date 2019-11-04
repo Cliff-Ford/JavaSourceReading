f59522088c4e7f0a7112f5b0d8824cdc864cfd7c
Cliff-Ford <473375811@qq.com>
2019-11-04 23:34:48 +0800

    开始阅读BigDecimal的代码

---
+++ src/main/java/read/java/math/ReadBigDecimal.java
@@ -0,0 +1,7 @@
+package read.java.math;
+
+public class ReadBigDecimal {
+    public static void main(String[] args) {
+        System.out.println("阅读java.lang.String源码，文章链接待补充");
+    }
+}
---
+++ src/test/java/use/java/math/UseBigDecimal.java
@@ -0,0 +1,7 @@
+package use.java.math;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class UseBigDecimal {
+}
