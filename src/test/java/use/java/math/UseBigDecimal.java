99ba732c04f61edf884aa630272a8d74316f4838
Cliff-Ford <473375811@qq.com>
2019-11-06 23:59:39 +0800

    添加BigDecimal的几个创建方法的使用代码

--- src/test/java/use/java/math/UseBigDecimal.java
+++ src/test/java/use/java/math/UseBigDecimal.java
@@ -1,7 +1,22 @@
 package use.java.math;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 @Slf4j
 public class UseBigDecimal {
+
+    @Test
+    public void howToNewBigDecimalObject(){
+        BigDecimal first = new BigDecimal(new char[]{'1', '2', '.', '3'}, 0, 4);
+        log.info(first.toString());
+        BigDecimal second = new BigDecimal(new char[]{'1', '2', '.', '3'}, 0, 4, MathContext.DECIMAL64);
+        log.info(second.toString());
+        BigDecimal third = new BigDecimal(new char[]{'1', '2', '.', '3'});
+        log.info(third.toString());
+
+    }
 }
