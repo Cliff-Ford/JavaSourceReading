152c2c3d3f0e564c12316e63c05d28807186ce6c
Cliff-Ford <473375811@qq.com>
2019-12-20 22:18:32 +0800

    添加文章链接

--- src/main/java/read/java/lang/ReadObject.java
+++ src/main/java/read/java/lang/ReadObject.java
@@ -2,6 +2,6 @@ package read.java.lang;
 
 public class ReadObject {
     public static void main(String[] args) {
-        System.out.println("试着阅读java.lang.Object源码，文章链接");
+        System.out.println("试着阅读java.lang.Object源码，文章链接https://juejin.im/post/5de33911f265da060c3bed6e");
     }
 }
